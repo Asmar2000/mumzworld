c18996d8cd2f2eea60835b01521a75cac53e7da0
Asmar2000 <M.L.NAGA@student.aast.edu>
2024-09-07 16:59:20 +0400

    Refactor e2e.js to add cypress-mochawesome-reporter dependency

--- cypress.config.js
+++ cypress.config.js
@@ -12,9 +12,19 @@ module.exports = defineConfig({
   viewportWidth: 1920,
   viewportHeight: 1080,
   "hideXHR": true,
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
+      
     },
   },
   
